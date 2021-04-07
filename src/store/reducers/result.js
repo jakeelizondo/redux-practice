6451ef93470eec008c210fd4c20613581577bc20
Jake Elizondo <jake.elizondo23@gmail.com>
2021-04-07 13:27:35 -0400

    cleaned up reducers with utility functions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,27 +1,29 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedResults = state.results.filter(
+    (result) => result.id !== action.elementId
+  );
+  return updateObject(state, { results: updatedResults });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({
           id: new Date(),
           value: action.result,
         }),
-      };
+      });
+
     case actionTypes.DELETE_RESULT:
-      const updatedResults = state.results.filter(
-        (result) => result.id !== action.elementId
-      );
-      return {
-        ...state,
-        results: updatedResults,
-      };
+      return deleteResult(state, action);
   }
 
   return state;
